93987a69eb2af46a7d900acb4be5573abdcb0089
Kc2Ne <1105665889@qq.com>
2021-02-01 22:15:32 +0800

    test

--- Hello world/test1.c
+++ Hello world/test1.c
@@ -1,11 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-int main()
-{
-	int num1 = 0;
-	int num2 = 0;
-	int sum = 0;
-	scanf_s("%d%d", &num1, &num2);
-	sum = num1 + num2;
-		printf("sum = %d\n", sum);
-		return 0;
-}
+
+//enum Colour
+//{
+//	RED,
+//	YELLOW,
+//	BLUE       //枚举常量
+//};
+//int main()
+//{
+//		printf("%d\n", RED);//默认值为0
+//		printf("%d\n", YELLOW);//默认值为1
+//		printf("%d\n", BLUE);//默认值为2
+//		return 0;
+//}
+
+//int main()
+//{
+//	int num = 4;
+//	printf("%d\n", num);
+//	num = 8;
+//	printf("%d\n", num);
+//}#define MAX 10
+//int main()
+//{
+//	int arr[MAX] = { 0 };
+//	printr("%d\n", MAX);
+//	return 0；
+//}
+
