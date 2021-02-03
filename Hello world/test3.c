cf04f37a28fcece69cc6cc0f5e6d6cf028ad6c31
Kc2Ne <1105665889@qq.com>
2021-02-03 23:47:32 +0800

    test

---
+++ Hello world/test3.c
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	char arr1[] = "a\0bc";
+//	printf("%d\n", strlen(arr1));
+//	return 0;
+//}
+
+//int main()
+//{
+//	int input = 0;
+//	printf("加入比特\n");
+//	printf("你要好好学习吗?(1/0)>:");
+//	scanf("%d", &input);
+//	if (input == 1)
+//		printf("好offer\n");
+//	else
+//		printf("卖红薯\n");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int line = 0;
+//	printf("加入比特\n");
+//	while (line < 20000)
+//	{
+//		printf("敲一行代码: %d\n", line);
+//		line++;
+//	}
+//	printf("good\n");
+//	return 0;
+//}
+
+//int Add(int x, int y)
+//{
+//	int z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int sum = 0;
+//	sum = Add(a, b);
+//	printf("sum = %d\n", sum);
+//	return 0;
+// } 
+
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	while(i < 10)
+//	{
+//		printf("%d\n", arr[i]);
+//		i++;
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 1;
+//	int b = a << 1;
+//	printf("%d\n", b);
+//	return 0;
+//}
