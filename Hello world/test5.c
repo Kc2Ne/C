23d3f750b1f6d24265675527296f127fbe1fd26b
Kc2Ne <1105665889@qq.com>
2021-02-06 22:33:08 +0800

    test

---
+++ Hello world/test5.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+ 
+//int main()
+//{
+//	double d = 3.14;
+//	double* pd = &d;
+//	*pd = 5.5;
+//	printf("%lf\n", d);
+//	return 0;
+//}
+//int main()
+//{
+//	short a = 10;
+//	printf("%d\n", sizeof(a));
+//	return 0;
+//}
+
+//struct book
+//{
+//		char name[20];
+//		short price;
+//		
+//		
+//}; 
+//
+//int main()
+//{
+//	struct book b1 = { "C语言程序设计", 55,  };//利用结构体类型创建一个该类型的结构体变量
+//	struct book* pb = &b1;
+//	//利用pb打印出名字和价格
+//	//操作符 .        结构体变量.成员
+//	//操作符 ->     结构体指针->成员
+//	printf("书名：%s\n", pb->name);
+//	printf("书名：%d\n", pb->price);
+//	//printf("书名：%s\n", (*pb).name); 
+//	//printf("书名：%d\n", (*pb).price);
+//	//printf("书名：%s\n", b1.name);
+//	//printf("价格：%d\n", b1.price);
+//	return 0;
+//}
+
